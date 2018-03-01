18c6d965c5e90850e455c88abc9300997c5ecb17
Josue Ramirez <joshua.rg18@gmail.com>
2018-02-28 21:19:31 -0600

    Create 01.Computadoras.cs

---
+++ 01.Computadoras.cs
@@ -0,0 +1,20 @@
+/** Una tienda se dedica a la venta de computadoras, cuenta con 10 vendedores. Cada uno de 
+ellos tiene un sueldo mensual pero además de su sueldo ganan una comisión por venta:
+
+	1. Si el monto de venta es mayor o igual de 1000 la comisión será del 15%
+	2. Si el monto de venta es mayor o igual a 500 y menor de 1000 la comisión es del 10%
+	3. Si el monto de la venta es menor que 500 el 5% de comisión
+
+Calcular cuánto recibirán por comisión y el salario total cada uno de los vendedores.  **/
+
+
+using System;
+{
+    class computadoras
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
