bb786f6fe76deebe1e64cee23510ffe1b60d156f
Josue Ramirez <joshua.rg18@gmail.com>
2018-02-28 21:21:29 -0600

    Create 02.Supermercado.cs

---
+++ 02.Supermercado.cs
@@ -0,0 +1,27 @@
+/** Un supermercado realiza una tómbola, en la cual tienen que sacar de una canasta una bolita 
+la cual tiene un número grabado, los premios se dan bajo la siguiente tabla y solamente tienen 
+derecho los clientes cuya compra supere los $30.
+
+-----------------------------------------------------
+|	Bolita		                  |				Premio				|
+-----------------------------------------------------
+|		1		|	                      1 shampoo						|
+|		2		|	          1 paquete de 3 jabones de baño	|
+|		3		|	                1 pasta de dientes				|
+|		4		|	                1 bolsa de detergente			|
+|		5		|	            1 caja de cereal de chocolate	|
+-----------------------------------------------------
+
+Notal: No se sabe la cantidad de clientes que llegará al supermercado.  **/
+
+using System;
+
+{
+    class supermercado
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
