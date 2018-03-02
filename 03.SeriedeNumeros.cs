48b24774d6bed9668d70476a1f4a472dbcdaf5ab
Josue Ramirez <joshua.rg18@gmail.com>
2018-03-01 22:40:35 -0600

    Update 03.SeriedeNumeros.cs

--- 03.SeriedeNumeros.cs
+++ 03.SeriedeNumeros.cs
@@ -1,12 +1,14 @@
 /**  Mostrar en pantalla la siguiente serie de números: 100 98 96 94... 56 54 52 50 **/
 
 using System;
+class serienumeros
 {
-    class serienumeros
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int Num;
+        for (Num = 100; Num >= 50; Num = Num - 2)
         {
-            Console.WriteLine("Hello World!");
-        }
+			Console.Write(Num + ", ");
+		}
     }
 }
