ae7d8ce796cf53dee0deae25e3720082753b395b
Josue Ramirez <joshua.rg18@gmail.com>
2018-03-01 22:33:10 -0600

    Create 03.SeriedeNumeros.cs

---
+++ 03.SeriedeNumeros.cs
@@ -0,0 +1,12 @@
+/**  Mostrar en pantalla la siguiente serie de números: 100 98 96 94... 56 54 52 50 **/
+
+using System;
+{
+    class serienumeros
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
