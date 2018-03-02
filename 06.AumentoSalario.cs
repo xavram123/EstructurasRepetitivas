5ff9b4d0244fcc36250db2120f5f1f8b6e051a42
Josue Ramirez <joshua.rg18@gmail.com>
2018-03-01 23:04:03 -0600

    Create 06.AumentoSalario.cs

---
+++ 06.AumentoSalario.cs
@@ -0,0 +1,19 @@
+/** En una empresa de 100 trabajadores, se hará un aumento al salario de acuerdo 
+con el tiempo de servicio, para este aumento se tomará en cuenta lo siguiente:
+
+Tiempo de servicio: de  1 a 5  años  >> Aumento: $25
+Tiempo de servicio: de  5 a 10 años  >> Aumento: $50
+Tiempo de servicio: de 10 a 20 años  >> Aumento: $100
+Tiempo de servicio: de 20 años a más >> Aumento: $150  **/
+
+using System;
+
+{
+    class aumentosalario
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
