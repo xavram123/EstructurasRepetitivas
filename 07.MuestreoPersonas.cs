20b05d688effbcf481c5310cada1920f90f855cf
Josue Ramirez <joshua.rg18@gmail.com>
2018-03-01 23:05:58 -0600

    Create 07.MuestreoPersonas.cs

---
+++ 07.MuestreoPersonas.cs
@@ -0,0 +1,24 @@
+/** Una persona debe realizar un muestreo con 50 personas para determinar el promedio 
+de peso de los niños, jóvenes, adultos y ancianos que existen en su zona habitacional, 
+se determinan las categorías con base en la siguiente tabla:
+
+-----------------------------------------
+|	Categorías   |			Edad		|
+-----------------------------------------
+|	Niños        |		 0 – 12			|
+|	Jóvenes      |		13 – 29			|
+|	Adultos      |		30 – 59			|
+|	Ancianos     |		60 en Adelante	|
+-----------------------------------------   **/
+
+using System;
+
+{
+    class muestreopersonas
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
