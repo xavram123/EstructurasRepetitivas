644754306250e1c7450efe01f84b56d95c21b500
Josue Ramirez <joshua.rg18@gmail.com>
2018-03-01 22:48:56 -0600

    Create 04.PrimerasPotencias.cs

---
+++ 04.PrimerasPotencias.cs
@@ -0,0 +1,17 @@
+/** Hacer un algoritmo que imprima las primeras 20 potencias de 3, de la siguiente manera:
+	
+	3 ^ 1 = 3
+	3 ^ 2 = 9
+	.
+	.
+	.
+	3 ^ 20 = 3486784401  **/
+
+using System;
+class primeraspotencias
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Hello World!");
+	}
+}
