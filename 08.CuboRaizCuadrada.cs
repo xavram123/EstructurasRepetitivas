9f70a0ba92744fa9a656483534aad5ed4998300c
Josue Ramirez <joshua.rg18@gmail.com>
2018-03-01 23:06:51 -0600

    Create 08.CuboRaizCuadrada.cs

---
+++ 08.CuboRaizCuadrada.cs
@@ -0,0 +1,16 @@
+/** Hacer un programa que imprima el cubo y la raíz cuadrada de un numero ingresado 
+por la pantalla; el programa debe estar confeccionado de tal manera que el proceso 
+a efectuarse sea repetitivo. Debe terminar cuando al pedir el número se ingrese el 
+valor 0. **/
+
+using System;
+
+{
+    class cuboraizcuadrada
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
