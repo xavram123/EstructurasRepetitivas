c960cfe4e055847ebd8e223579fbcee35dc06cdc
Josue Ramirez <joshua.rg18@gmail.com>
2018-03-01 22:29:09 -0600

    Update 02.Supermercado.cs

--- 02.Supermercado.cs
+++ 02.Supermercado.cs
@@ -2,12 +2,15 @@
 la cual tiene un número grabado, los premios se dan bajo la siguiente tabla y solamente tienen 
 derecho los clientes cuya compra supere los $30.
 
-|	1	|	    1 shampoo                       |
-|	2	|	    1 paquete de 3 jabones de baño  |
-|	3	|	    1 pasta de dientes              |
-|	4	|	    1 bolsa de detergente           |
-|	5	|	    1 caja de cereal de chocolate   |
--------------------------------------------------
+---------------------------------------------
+|Bola   |   Premio                          |
+---------------------------------------------
+|	1	|   1 shampoo                       |
+|	2	|   1 paquete de 3 jabones de baño  |
+|	3	|   1 pasta de dientes              |
+|	4	|   1 bolsa de detergente           |
+|	5	|   1 caja de cereal de chocolate   |
+---------------------------------------------
 
 Notal: No se sabe la cantidad de clientes que llegará al supermercado.  **/
 
