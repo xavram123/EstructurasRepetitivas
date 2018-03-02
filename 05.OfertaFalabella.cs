6fe69e428f2cfa9f8b8665bdabf6932c26241021
Josue Ramirez <joshua.rg18@gmail.com>
2018-03-01 23:00:54 -0600

    Create 05.OfertaFalabella.cs

---
+++ 05.OfertaFalabella.cs
@@ -0,0 +1,22 @@
+/** "SAGA FALABELLA" se encuentra de aniversario y ha programado una serie de ofertas con 
+la finalidad de brindar facilidades a sus clientes y a la vez de incrementar sus ventas. 
+Estas ofertas se basan específicamente en un porcentaje de descuento sobre el total de 
+compra el cual varía de acuerdo al monto:
+
+	1. Por un monto mayor o igual a $500 se hará un descuento del 30%
+	2. Por un monto menor de $500 pero mayor o igual a 200 se hará un descuento del 20%
+	3. Por un monto menor de $200 pero mayor o igual a 100 se hará un descuento del 10%
+
+No se conoce el número de personas que llegarán a la tienda. **/
+
+using System;
+
+{
+    class ofertafalabella
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
