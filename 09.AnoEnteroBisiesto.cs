37ed994c53ffc1dd0ee3f5759f93960363f218e8
Josue Ramirez <joshua.rg18@gmail.com>
2018-03-01 23:10:14 -0600

    Create 09.AnoEnteroBisiesto.cs

---
+++ 09.AnoEnteroBisiesto.cs
@@ -0,0 +1,15 @@
+/** Ingresar año como entero. Mostrar si el año es bisiesto. Un año es bisiesto 
+si el año es divisible por 4 y no es divisible por 100 o es divisible por 400. 
+El lazo deberá ser controlado por el usuario. **/
+
+using System;
+
+{
+    class anoenterobisiesto
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
