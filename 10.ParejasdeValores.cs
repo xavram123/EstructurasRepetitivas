3178ae7ae9e1d0accc18270449e8cbbfb320f97a
Josue Ramirez <joshua.rg18@gmail.com>
2018-03-01 23:11:31 -0600

    Create 10.ParejasdeValores.cs

---
+++ 10.ParejasdeValores.cs
@@ -0,0 +1,26 @@
+/** Hacer un algoritmo que imprima las parejas de valores:
+
+	| 3 | 2 |
+	| 3 | 4 |
+	| 3 | 6 |
+	| 3 | 8 |
+	| 6 | 2 |
+	| 6 | 4 |
+	| 6 | 6 |
+	| 6 | 8 |
+	| 9 | 2 |
+	| 9 | 4 |
+	| 9 | 6 |
+	| 9 | 8 |   **/
+
+using System;
+
+{
+    class parejasdevalores
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
