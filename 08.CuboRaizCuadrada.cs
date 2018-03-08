7149eda5e44c6a1029d5fd8cb754ca939e5f6e54
xavram123 <36433357+xavram123@users.noreply.github.com>
2018-03-07 23:21:36 -0600

    Update 08.CuboRaizCuadrada.cs

--- 08.CuboRaizCuadrada.cs
+++ 08.CuboRaizCuadrada.cs
@@ -4,13 +4,31 @@ a efectuarse sea repetitivo. Debe terminar cuando al pedir el número se ingrese
 valor 0. **/
 
 using System;
-
+class cubo
 {
-    class cuboraizcuadrada
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+static void Main(String [] xavi)
+{
+double raiz,cubo,num,c;
+c=0;
+
+do
+{
+	c=c+1;
+	Console.WriteLine("PROCESO Nยบ{0}:", c);
+	Console.Write("Ingrese numero ");
+	num=Convert.ToDouble(Console.ReadLine());
+	if(num != 0)
+	{
+	cubo = Math.Pow(num,3);
+	raiz = Math.Sqrt(num);	
+	
+	Console.WriteLine("El cubo es: " +cubo);
+	Console.WriteLine("La raiz es: " +raiz);
+	}
+}
+while (num != 0);
+{
+Console.Write("Fin del proceso: ");
+}
+}
 }
